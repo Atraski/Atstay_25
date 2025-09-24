8fa90d1b5f17e2bc4d07f8980a7f731c04190fb4
PAWAN KUMAR <pawanupadhyaay@gmail.com>
2025-09-24 11:52:29 +0530

    server.js changes

--- server/server.js
+++ server/server.js
@@ -24,3 +24,6 @@ app.get("/", (req, res) =>res.send("API is running..."));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+export default app;
